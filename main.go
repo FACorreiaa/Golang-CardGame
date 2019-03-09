511102d5362142574e85f782cae32be79791f99b
Knox316 <fernandocorreia316@gmail.com>
2019-03-09 19:51:29 +0000

    error handling

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
 	// cards := newDeck()
 
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.print()
-	// remainingCards.print()
-	cards := newDeck()
-	fmt.Println(cards.toString())
+	// // remainingCards.print()
+
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
+
 }
