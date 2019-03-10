3ea005a9c9bd38b06b25263f4e2e009440927029
Knox316 <fernandocorreia316@gmail.com>
2019-03-10 10:11:56 +0000

    added testing with Golang

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ func main() {
 	// fmt.Println(cards.toString())
 	// cards.saveToFile("my_cards")
 
-	cards := newDeckFromFile("my_cards")
-	cards.print()
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
 
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
